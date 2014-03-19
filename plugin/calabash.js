6baa2d163f8b9585b7ad3a833b50800800e54dbd
Matt Apperson <matt@appersonlabs.com>
2014-03-18 20:31:04 -0400

    just picky about my === 😊

--- plugin/calabash.js
+++ plugin/calabash.js
@@ -32,33 +32,33 @@ exports.run = function(logger, config, cli, finished) {
     var passthroughCommands = ['build'].concat(cli.argv['$_'].slice(3));
 
 
-    if(platform == 'android') {
+    if(platform === 'android') {
 
     }
 
-	if(platform == 'iphone' || platform == 'ios'){
+	if(platform == 'iphone' || platform === 'ios'){
 		console.info("yes. it is iphone alright");
-		
-		
+
+
 		/****
 		#do a clean first then build and run calabash-ios setup
 		cd build/iphone && calabash-ios setup
-		
+
 		rm -fR features #relative to build/iphone
 		ln -s ../../features features
-		
+
 		xcodebuild -project alloy_fugitive.xcodeproj -configuration Debug -target alloy_fugitive-cal  -sdk iphonesimulator
 		****/
 	}
 
 	/* okay. I know this is crap code, but if they are not using ios or android, this command should gracefully bow out*/
-	if(platform != 'android' && platform != 'ios' && platform ='iphone' ) {
+	if(platform !== 'android' && platform !== 'ios' && platform !=='iphone' ) {
 		console.info('Calabash does not support your kind. \n If you are doing mobile web, this statement is a lie and Andrew is just being lazy atm.');
     }
 
     // pass through the command args to Ti `build` commands
    /** exec('ti', passthroughCommands, null, function(output) {
-	   
+
         console.info(output);
     });
 
