ebabf9ee5577e0694c9e09e9f54935b21bde7a11
Andrew McElroy <sophrinix@gmail.com>
2014-03-18 19:06:14 -0500

    baby steps on the actual implementation

--- plugin/calabash.js
+++ plugin/calabash.js
@@ -28,15 +28,44 @@ exports.config = function() {
  * @param {Function} finished - Callback when the command finishes
  */
 exports.run = function(logger, config, cli, finished) {
-    var platform = cli.argv.platform || cli.argv.p || 'ios';
+    var platform = cli.argv.platform || cli.argv.p ;
     var passthroughCommands = ['build'].concat(cli.argv['$_'].slice(3));
 
-    if(platform !== 'ios') {
+
+    if(platform == 'android') {
 
     }
 
-    // pass through the command args to Ti `buid` commands
-    exec('ti', passthroughCommands, null, function(output) {
+	if(platform == 'iphone' || platform == 'ios'){
+		console.info("yes. it is iphone alright");
+		
+		
+		/****
+		#do a clean first then build and run calabash-ios setup
+		cd build/iphone && calabash-ios setup
+		
+		rm -fR features #relative to build/iphone
+		ln -s ../../features features
+		
+		xcodebuild -project alloy_fugitive.xcodeproj -configuration Debug -target alloy_fugitive-cal  -sdk iphonesimulator
+		****/
+	}
+
+	/* okay. I know this is crap code, but if they are not using ios or android, this command should gracefully bow out*/
+	if(platform != 'android' && platform != 'ios' && platform ='iphone' ) {
+		console.info('Calabash does not support your kind. \n If you are doing mobile web, this statement is a lie and Andrew is just being lazy atm.');
+    }
+
+    // pass through the command args to Ti `build` commands
+   /** exec('ti', passthroughCommands, null, function(output) {
+	   
         console.info(output);
     });
+
+**/
+
+
+
+
+
 };
