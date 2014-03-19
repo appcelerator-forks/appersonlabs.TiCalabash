da08dd7cb1d0eebc3b1ef193dbbdeeb12570c1dc
Matt Apperson <matt@appersonlabs.com>
2014-03-18 21:36:44 -0400

    checking all the things

--- plugin/calabash.js
+++ plugin/calabash.js
@@ -1,7 +1,9 @@
 /*jslint node: true */
 'use strict';
 
-var exec = require('../lib/exec')
+var exec = require('../lib/exec'),
+    fs = require('fs'),
+    path = require('path');
 /** command description. */
 exports.cliVersion = '>=3.2.1';
 exports.title = 'Build w/Calabash';
@@ -27,14 +29,27 @@ exports.config = function() {
  * @param {Function} finished - Callback when the command finishes
  */
 exports.run = function(logger, config, cli, finished) {
-    var platform = (cli.argv.platform || cli.argv.p);
+    var platform = (cli.argv.platform || cli.argv.p),
+        projectDir = process.env.SOURCE_ROOT ? path.join(process.env.SOURCE_ROOT, '..', '..') : '.';
 
     /* if they are not using ios or android, this command should gracefully bow out*/
-    if( ['android', 'ios', 'iphone'].indexOf( platform ) === -1 ) {
+    if (['android', 'ios', 'iphone'].indexOf(platform) === -1) {
         throw 'Calabash does not support your kind. \n If you are doing mobile web, this statement is a lie and Andrew is just being lazy atm.';
     }
 
-    // require and run the correct platform...
-    require('../lib/run_' + (platform === 'iphone' ? 'ios' : platform ))(logger, config, cli, finished);
+    fs.exists(path.join(projectDir, 'tiapp.xml'), function(project_exists) {
+        if (project_exists) {
 
+            fs.exists(path.join(projectDir, 'app'), function(is_alloy) {
+                if (is_alloy) {
+                    // do alloy crap here...
+                }
+
+                // require and run the correct platform...
+                require('../lib/run_' + (platform === 'iphone' ? 'ios' : platform))(logger, config, cli, finished);
+            });
+        } else {
+            throw "Invalid Titanium project location";
+        }
+    });
 };
