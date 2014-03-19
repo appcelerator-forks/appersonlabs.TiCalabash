095b9f17b8e30111d2f33913cd28b438999dec8d
Matt Apperson <matt@appersonlabs.com>
2014-03-18 21:14:40 -0400

    Better code org

--- plugin/calabash.js
+++ plugin/calabash.js
@@ -1,8 +1,7 @@
 /*jslint node: true */
 'use strict';
 
-var exec = require('../lib/exec');
-
+var exec = require('../lib/exec')
 /** command description. */
 exports.cliVersion = '>=3.2.1';
 exports.title = 'Build w/Calabash';
@@ -28,46 +27,14 @@ exports.config = function() {
  * @param {Function} finished - Callback when the command finishes
  */
 exports.run = function(logger, config, cli, finished) {
-    var platform = cli.argv.platform || cli.argv.p,
-        passthroughCommands = ['build'].concat(cli.argv['$_'].slice(3));
+    var platform = (cli.argv.platform || cli.argv.p);
 
     /* if they are not using ios or android, this command should gracefully bow out*/
     if( ['android', 'ios', 'iphone'].indexOf( platform ) === -1 ) {
         throw 'Calabash does not support your kind. \n If you are doing mobile web, this statement is a lie and Andrew is just being lazy atm.';
     }
 
-
-    if(platform === 'android') {
-
-    }
-
-	if(platform == 'iphone' || platform === 'ios'){
-		console.info("yes. it is iphone alright");
-
-
-		/**
-		#do a clean first then build and run calabash-ios setup
-		cd build/iphone && calabash-ios setup
-
-		rm -fR features #relative to build/iphone
-		ln -s ../../features features
-
-		xcodebuild -project alloy_fugitive.xcodeproj -configuration Debug -target alloy_fugitive-cal  -sdk iphonesimulator
-		****/
-	}
-
-
-
-    // pass through the command args to Ti `build` commands
-   /** exec('ti', passthroughCommands, null, function(output) {
-
-        console.info(output);
-    });
-
-**/
-
-
-
-
+    // require and run the correct platform...
+    require('../lib/run_' + (platform === 'iphone' ? 'ios' : platform ))(logger, config, cli, finished);
 
 };
