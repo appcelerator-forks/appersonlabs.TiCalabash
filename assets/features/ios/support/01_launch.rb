dda497e30b4e43f5fc8c9e3270168a4270788aa5
Andrew McElroy <sophrinix@gmail.com>
2014-10-12 16:26:06 -0400

    removed the suck... added working software for ios 8 and android

--- assets/features/ios/support/01_launch.rb
+++ assets/features/ios/support/01_launch.rb
@@ -17,18 +17,19 @@
 require 'calabash-cucumber/launcher'
 
 
+# APP_BUNDLE_PATH = "~/Library/Developer/Xcode/DerivedData/??/Build/Products/Calabash-iphonesimulator/??.app"
+# You may uncomment the above to overwrite the APP_BUNDLE_PATH
+# However the recommended approach is to let Calabash find the app itself
+# or set the environment variable APP_BUNDLE_PATH
+
+
 Before do |scenario|
   @calabash_launcher = Calabash::Cucumber::Launcher.new
-  scenario_tags = scenario.source_tag_names
-  if scenario_tags.include?('@reinstall')
-    @calabash_launcher.reset_app_jail
-  end
   unless @calabash_launcher.calabash_no_launch?
     @calabash_launcher.relaunch
     @calabash_launcher.calabash_notify(self)
   end
 end
-
 
 After do |scenario|
   unless @calabash_launcher.calabash_no_stop?
@@ -36,5 +37,12 @@ After do |scenario|
     if @calabash_launcher.active?
       @calabash_launcher.stop
     end
+  end
+end
+
+at_exit do
+  launcher = Calabash::Cucumber::Launcher.new
+  if launcher.simulator_target?
+    Calabash::Cucumber::SimulatorHelper.stop unless launcher.calabash_no_stop?
   end
 end
